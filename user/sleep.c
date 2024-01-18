8bc711ba95ec91b141b3213beede0c436f2605b9
Qi Wang <mpiglet@outlook.com>
2024-01-18 23:30:20 +0800

    chap1: sleep exercise done

---
+++ user/sleep.c
@@ -0,0 +1,15 @@
+
+#include "kernel/types.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[]) {
+  int secs;
+  if (argc != 2) {
+    printf("usage: sleep #");
+    exit(0);
+  }
+
+  secs = atoi(argv[1]);
+  sleep(secs);
+  exit(0);
+}
